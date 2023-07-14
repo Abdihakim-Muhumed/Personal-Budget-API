d935ad80fd5fd6225d8140acd4b09e17b3f6a60b
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2023-07-14 11:16:07 +0300

    Added endpoint to retrieve specific envelope by di

--- envelopes-router.js
+++ envelopes-router.js
@@ -14,4 +14,13 @@ envelopesRouter.get('/', (req, res) => {
     const envelopes = getAllEnvelopes()
     res.status(200).json(envelopes)
 })
+
+envelopesRouter.get('/:id', (req, res) => {
+    try {
+        const envelope = getEnvelopeById(req.params.id)
+        res.status(200).json(envelope)
+    } catch (error) {
+        res.status(404).send()
+    }
+})
 module.exports = envelopesRouter
