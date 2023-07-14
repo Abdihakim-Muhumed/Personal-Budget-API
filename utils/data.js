1ff1a6653076d91b977f0e9b67dcee925601d0c5
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2023-07-14 09:19:43 +0300

    Created array of envelope objects

---
+++ utils/data.js
@@ -0,0 +1,30 @@
+const envelopes = [
+    {
+        id: 0,
+        title: 'Groceries',
+        description: 'This envelope will be used to budget for Grocery items like spinach, tomatoes e.t.c',
+        budget: 100,
+        balance: 100
+    },
+    {
+        id: 1,
+        title: 'Gas',
+        description: 'This envelope will be used to budget for gas for the car',
+        budget: 100,
+        balance: 100
+    },
+    {
+        id: 2,
+        title: 'Clothing',
+        description: 'This envelope will be used to budget for clothes and shoes',
+        budget: 100,
+        balance: 100
+    },
+    {
+        id: 3,
+        title: 'Dining Out',
+        description: 'This envelope will be used to budget for outings and street foods',
+        budget: 100,
+        balance: 100
+    },
+]
