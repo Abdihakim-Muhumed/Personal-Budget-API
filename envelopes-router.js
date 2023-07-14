d9535c60ba45fc9011f46f65b02cc6044c799de1
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2023-07-14 12:11:06 +0300

    Added endpoint to delete an envelope

--- envelopes-router.js
+++ envelopes-router.js
@@ -20,7 +20,7 @@ envelopesRouter.get('/:id', (req, res) => {
         const envelope = getEnvelopeById(req.params.id)
         res.status(200).json(envelope)
     } catch (error) {
-        res.status(404).send()
+        res.status(404).send(error.message)
     }
 })
 
@@ -28,6 +28,15 @@ envelopesRouter.post('/', (req, res) => {
     try {
         const newEnvelope = addNewEnvelope(req.query)
         res.status(201).send(newEnvelope)
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
+})
+
+envelopesRouter.delete('/:id', (req, res) => {
+    try {
+        const deleted = deleteEnvelope(req.params.id)
+        res.status(200).send(deleted)
     } catch (error) {
         res.status(404).send(error.message)
     }
