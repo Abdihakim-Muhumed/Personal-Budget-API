e0e012f465a36a2ab4e6f58ea5c407c8b3a0116f
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2023-07-16 13:02:22 +0300

    Added more constraints

--- db/tables.sql
+++ db/tables.sql
@@ -1,14 +1,14 @@
 CREATE TABLE "envelopes" (
   "id" SERIAL PRIMARY KEY,
   "title" varchar(20) UNIQUE,
-  "allocated_budget" money CHECK(allocated_budget > balance),
-  "balance" money,
-  "created_at" timestamp
+  "allocated_budget" money,
+  "balance" money CHECK(balance <= allocated_budget),
+  "created_at" timestamp DEFAULT now()
 );
 
 CREATE TABLE "transactions" (
   "id" SERIAL PRIMARY KEY,
-  "date" date,
+  "date" date DEFAULT CURRENT_DATE,
   "amount" money,
   "envelope_id" integer NOT NULL,
   "reciepient" varchar(20)
