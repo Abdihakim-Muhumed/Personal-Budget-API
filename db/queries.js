08b26546b9d9fcbf72db8b8a8511849004c05d56
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2023-09-08 14:52:29 +0300

    Debug delete endpoint

--- db/queries.js
+++ db/queries.js
@@ -1,5 +1,10 @@
 const Pool = require('pg').Pool
 const DB_CONFIG = require('../config.js').DB_CONFIG;
+const {
+    substractFromBudget,
+    addToBudget
+} = require('./utility-queries.js')
+
 
 const pool = new Pool(DB_CONFIG);
 
@@ -16,3 +21,171 @@ const sampleQuery = () => {
     )
 }
 sampleQuery()
+
+const getAllEnvelopes = (req, res) =>{
+    pool.query(
+        'SELECT * FROM envelopes ORDER BY id',
+        (error, results) => {
+            if(error){
+                throw error
+            }
+            res.status(200).json(results.rows)
+        }
+    )
+}
+
+const getEnvelopeById = (req, res) => {
+    const id = req.params.id
+    pool.query(
+        'SELECT * FROM envelopes WHERE id = $1',
+        [id],
+        (error, results) => {
+            if(error){
+                throw error
+            }
+            if(results.rows.length < 1){
+                res.status(404).send('Invalid ID!')
+            }else{
+                res.status(200).json(results.rows)
+            }
+        }
+    )
+}
+
+const addNewEnvelope = (req, res) => {
+    const {title, allocated_budget} = req.query
+    pool.query(
+        'INSERT INTO envelopes(title, allocated_budget, balance) VALUES( $1, $2, $2) RETURNING *',
+        [title, allocated_budget],
+        (error, results) => {
+            if(error){
+                res.status(403).send(error.message)
+            }
+            res.status(201).send(results.rows)
+        }
+    )
+}
+
+const deleteEnvelope = (req, res) => {
+    const id = req.params.id
+    pool.query(
+        'DELETE FROM envelopes WHERE id = $1 RETURNING *;',
+        [id],
+        (error, results) => {
+            if(error){
+                res.status(404).send()
+            }
+            if(!results.rows[0]){
+                res.status(403).send('Nothing to delete. Envelope with specified ID does not exist!')
+            }
+            res.status(200).send('Envelope deleted deleted with ID: '+id)
+        }
+    )
+}
+const updateEnvelope = (req, res,) => {
+    const id = req.params.id
+    const {title, allocated_budget} = req.query
+    if(title && allocated_budget){
+        pool.query(
+            'UPDATE envelopes SET title = $2 WHERE id=$1 RETURNING *;',
+            [id, title],
+            (error, results) => {
+                if(error){
+                    throw error
+                }
+                console.log(results.rows[0])
+                pool.query(
+                    'SELECT * FROM envelopes WHERE id=$1; ',
+                    [id],
+                    (error, results) => {
+                        if(error){
+                            throw error
+                        }
+                        if((results.rows[0].allocated_budget) < allocated_budget){
+                            let difference = allocated_budget - results.rows[0].allocated_budget
+                            const addToBudget = addToBudget(difference, id, res)
+                        }
+                        if((results.rows[0].allocated_budget) > allocated_budget){
+                            let difference = results.rows[0].allocated_budget -allocated_budget
+                            const addToBudget = substractFromBudget(difference, id, res)
+                        }
+                    }
+                )
+            }
+        )    }
+    if(title && !allocated_budget){
+        pool.query(
+            'UPDATE envelopes SET title = $2 WHERE id=$1 RETURNING *;',
+            [id, title],
+            (error, results) => {
+                if(error){
+                    throw error
+                }
+                console.log(results.rows[0])
+                res.status(200).send(results.rows[0])
+            }
+        )
+    }
+    if(allocated_budget && !title){
+        pool.query(
+            'SELECT * FROM envelopes WHERE id=$1;',
+            [id],
+            (error, results) => {
+                if(error){
+                    throw error
+                }
+                if((results.rows[0].allocated_budget) < allocated_budget){
+                    let difference = allocated_budget - results.rows[0].allocated_budget
+                    const addToBudget = addToBudget(difference, id, res)
+                }
+                if((results.rows[0].allocated_budget) > allocated_budget){
+                    let difference = results.rows[0].allocated_budget -allocated_budget
+                    const addToBudget = substractFromBudget(difference, id, res)
+                }
+            }
+        )
+    }
+}
+
+const spendFromEnvelope = (req, res) => {
+    if(amount){
+        pool.query(
+            'SELECT balance FROM envelopes WHERE id=$1;',
+            [id],
+            (error, results) => {
+                if(error){
+                    throw error
+                }
+                if(results.rows.length < 1){
+                    res.status(404).send('Invalid ID!')
+                }else{
+                    if((results.rows[0].balance - amount) < 0){
+                        res.status(403).send('Not enough balance in this envelope!')
+                    }
+                    else{
+                        pool.query(
+                            'UPDATE envelopes SET balance = balance - $2 WHERE id = $1 RETURNING *;',
+                            [id, amount],
+                            (error, results) => {
+                                if(error){
+                                    throw error
+                                }
+                                console.log(results.rows[0])
+                                next()
+                            }
+                        )
+                    }
+                }
+            }
+
+        )
+    }
+}
+
+module.exports = {
+    getAllEnvelopes,
+    getEnvelopeById,
+    addNewEnvelope,
+    deleteEnvelope, 
+    updateEnvelope,
+}
