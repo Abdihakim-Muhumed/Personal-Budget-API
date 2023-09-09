2ec1cf0562c1958909821b394980379e2f77735e
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2023-09-09 16:39:07 +0300

    Set up transactions Router

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express')
 const app  =  express()
 const PORT  = 3000
 const envelopesRouter = require('./envelopes-router.js')
+const transactionsRouter = require('./transactionsRouter.js')
 const bodyParser = require('body-parser')
 
 app.use('/envelopes', envelopesRouter)
-
+app.use('/transactions', transactionsRouter)
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
---
+++ transactions-router.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const transactionsRouter = express.Router()
+
+
+
+module.exports = transactionsRouter;
