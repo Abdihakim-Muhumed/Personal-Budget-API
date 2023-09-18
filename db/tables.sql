61a38b56b60ab1a2c2df19907c591b6580d7c5b5
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2023-09-18 09:44:12 +0300

    Edit db

--- db/tables.sql
+++ db/tables.sql
@@ -1,15 +1,14 @@
 CREATE TABLE "envelopes" (
   "id" SERIAL PRIMARY KEY,
   "title" varchar(20) UNIQUE,
-  "allocated_budget" money,
-  "balance" money CHECK(balance <= allocated_budget),
+  "balance" integer,
   "created_at" timestamp DEFAULT now()
 );
 
 CREATE TABLE "transactions" (
   "id" SERIAL PRIMARY KEY,
   "date" date DEFAULT CURRENT_DATE,
-  "amount" money,
+  "amount" integer,
   "envelope_id" integer NOT NULL,
   "reciepient" varchar(20)
 );
