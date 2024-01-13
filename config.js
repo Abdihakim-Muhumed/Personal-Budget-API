ab5215011f2c0fdac6176b736116b7938cb90517
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2024-01-13 11:52:22 +0300

    Deployment

--- config.js
+++ config.js
@@ -1,13 +1,21 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
+const NODE_ENV = process.env.NODE_ENV;
+
 // DB_CONFIG
-const DB_CONFIG = {
-    username: process.env.USERNAME,
-    host: process.env.HOST,
-    database: process.env.DB,
-    password: process.env.PASSWORD, 
-    port:process.env.DB_PORT
+if(NODE_ENV=='development'){
+    const DB_CONFIG = {
+        username: process.env.USERNAME,
+        host: process.env.HOST,
+        database: process.env.DB,
+        password: process.env.PASSWORD, 
+        port:process.env.DB_PORT
+    }
+}else{
+    const DB_CONFIG = {
+        connectionString: process.env.DATABASEURL,
+    }
 }
 module.exports = {
     DB_CONFIG,
